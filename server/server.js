ff5dee6c1b1047a054f6e6ed30b156e2f46c46c6
Anthony Carranza <anthonycarranza123@gmail.com>
2024-11-27 10:06:39 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        connectSrc: ["'self'", "https://storage.googleapis.com"], // Allow fetch requests to Google Cloud Storage
+        connectSrc: ["'self'", "https://storage.googleapis.com"], // Allow fetch requests
         imgSrc: ["'self'", "https://storage.googleapis.com", "data:"],
         scriptSrc: ["'self'"],
         styleSrc: ["'self'", "https://fonts.googleapis.com", "'unsafe-inline'"],
